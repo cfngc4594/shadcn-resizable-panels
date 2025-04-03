e28ffba53a4d415761d527974cc9edbb1f592052
cfngc4594 <cfngc4594@gmail.com>
2025-04-03 17:48:16 +0800

    chore(deps): add dockview package

---
+++ src/app/(app)/@Code/page.tsx
@@ -0,0 +1,3 @@
+export default function CodePage() {
+  return <div className="px-4">Code</div>;
+}
--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,86 +1,37 @@
-"use client";
-
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
-import { useEffect, useState } from "react";
-import { Skeleton } from "@/components/ui/skeleton";
+import DockView from "@/components/dock-view";
 import { ModeToggle } from "@/components/mode-toggle";
-import { usePanelStore } from "@/stores/usePanelStore";
-import PanelToggleGroup from "@/components/panel-toggle-group";
 
 interface AppLayoutProps {
-  chat: React.ReactNode;
-  problem: React.ReactNode;
-  workspace: React.ReactNode;
+  Description: React.ReactNode;
+  Solutions: React.ReactNode;
+  Submissions: React.ReactNode;
+  Code: React.ReactNode;
+  Testcase: React.ReactNode;
+  TestResult: React.ReactNode;
 }
 
-export default function AppLayout({ chat, problem, workspace }: AppLayoutProps) {
-  const [mounted, setMounted] = useState(false);
-  const { isChatVisible, isProblemVisible, isWorkspaceVisible } = usePanelStore();
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
+export default function AppLayout({
+  Description,
+  Solutions,
+  Submissions,
+  Code,
+  Testcase,
+  TestResult,
+}: AppLayoutProps) {
   return (
-    <div className="flex flex-col h-full">
-      <header className="flex h-12 items-center justify-end gap-2 px-2.5">
-        <PanelToggleGroup />
+    <div className="flex flex-col h-screen">
+      <header className="flex items-center justify-end flex-none h-12 gap-2 px-2.5">
         <ModeToggle />
       </header>
-
-      <main className="flex flex-grow overflow-y-hidden p-2.5 pt-0">
-        {mounted ? (
-          <ResizablePanelGroup
-            autoSaveId="conditional"
-            direction="horizontal"
-          >
-            {isProblemVisible && (
-              <ResizablePanel
-                id="problem"
-                order={1}
-                className="border rounded-lg px-4 py-2"
-              >
-                {problem}
-              </ResizablePanel>
-            )}
-
-            {isProblemVisible && isWorkspaceVisible && (
-              <ResizableHandle className="mx-1 bg-transparent hover:bg-blue-500" />
-            )}
-
-            {isWorkspaceVisible ? (
-              <ResizablePanel
-                id="workspace"
-                order={2}
-                className="border rounded-lg px-4 py-2"
-              >
-                {workspace}
-              </ResizablePanel>
-            ) : (
-              <ResizableHandle className="mx-1 bg-transparent hover:bg-blue-500" />
-            )}
-
-            {isWorkspaceVisible && isChatVisible && (
-              <ResizableHandle className="mx-1 bg-transparent hover:bg-blue-500" />
-            )}
-
-            {isChatVisible && (
-              <ResizablePanel
-                id="chat"
-                order={3}
-                className="border rounded-lg px-4 py-2"
-              >
-                {chat}
-              </ResizablePanel>
-            )}
-          </ResizablePanelGroup>
-        ) : (
-          <Skeleton className="h-full w-full" />
-        )}
+      <main className="flex flex-grow overflow-y-hidden">
+        <DockView
+          Description={Description}
+          Solutions={Solutions}
+          Submissions={Submissions}
+          Code={Code}
+          Testcase={Testcase}
+          TestResult={TestResult}
+        />
       </main>
     </div>
   );
